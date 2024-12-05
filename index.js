4a91f05dddea8b20107a99d293e81ac7a0b734e7
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2024-12-05 09:51:32 +0100

    Traitement synchrone et asycnchrone avec setTimeout

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ function OnClick()
     let div = document.querySelector( "#main") ;
     div.innerHTML = div.innerHTML + "<br>" + valeur ;
 }
+
+function attendre( temps )
+{
+    let somme = 0 ;
+    for( let i=0; i<temps*1000000; i++ )
+    {
+        somme += i ;
+    }
+    return somme ;
+}
+
+function traitement()
+{
+    let resultat = attendre( 5000 ) ;
+
+    let div = document.querySelector( "#main") ;
+    div.innerHTML = div.innerHTML + "<br>Resultat: " + resultat ;
+}
+
+function tasync()
+{
+    setTimeout( function()
+    {
+        let div = document.querySelector( "#main") ;
+        div.innerHTML = div.innerHTML + "<br>2 secondes plus tard!" ;    
+
+        setTimeout( function()
+        {
+            let div = document.querySelector( "#main") ;
+            div.innerHTML = div.innerHTML + "<br>3 secondes plus tard!" ;  
+            
+            setTimeout( function()
+            {
+                let div = document.querySelector( "#main") ;
+                div.innerHTML = div.innerHTML + "<br>1 seconde plus tard!" ;      
+            }, 1000)
+        }, 3000) ;
+    }, 2000 ) ;
+}
